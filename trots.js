26f52b0dca34dab328d4f44513e4d292bbe7b455
TonyKimeu <TonyKimeu@users.noreply.github.com>
2017-02-17 09:01:46 +0300

    Add files via upload

--- trots.js
+++ trots.js
@@ -1,6 +1,9 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
