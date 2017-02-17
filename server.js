986be8afbe386e603aade033d3e2df3421d8c097
TonyKimeu <TonyKimeu@users.noreply.github.com>
2017-02-17 07:47:43 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var builder = require('botbuilder');
+var http = require('http');
+var querystring  = require('querystring');
+var restify = require('restify');
+
+var dotenv = require('dotenv');
+dotenv.load();
+
+var ConnectorBot = require('./connector');
+
+var connectorBot = ConnectorBot.start();
+
